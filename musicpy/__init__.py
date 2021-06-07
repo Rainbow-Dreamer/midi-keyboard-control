44bc5a1811388db275d9242209e9b26c330fcdae
Rainbow Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2021-06-08 00:28:44 +0800

    update

--- musicpy/__init__.py
+++
@@ -1,1 +0,0 @@
-from .musicpy import *
---
+++ packages/config.py
@@ -0,0 +1,118 @@
+# settings
+midi_keyboard_detect_interval = 10
+repeat_interval = 0.3
+mouse_move_distance = 30
+mouse_move_duration = 0.1
+screen_size = (900, 500)
+fonts_size = 20
+fonts = 'Consolas'
+bold = ''
+
+key_settings = {
+    'print screen': 'A1',
+    'scroll lock': 'A#1',
+    'insert': 'B1',
+    'home': 'C2',
+    'page up': 'C#2',
+    'delete': 'D2',
+    'end': 'D#2',
+    'page down': 'E2',
+    'esc': 'F2',
+    '`': 'F#2',
+    '=': 'G2',
+    '\\': 'G#2',
+    'ctrl': 'A2',
+    'alt': 'A#2',
+    'tab': 'B2',
+    'mouse move up': 'C3',
+    'mouse move down': 'C#3',
+    'mouse move left': 'D3',
+    'mouse move right': 'D#3',
+    'mouse middle click': 'E3',
+    'mouse left click': 'F3',
+    'mouse right click': 'F#3',
+    '/': 'G3',
+    '[': 'G#3',
+    ']': 'A3',
+    'left': 'A#3',
+    'right': 'B3',
+    'up': 'C4',
+    'down': 'C#4',
+    ';': 'D4',
+    "'": 'D#4',
+    ',': 'E4',
+    '.': 'F4',
+    "caps lock": 'F#4',
+    'win': 'G4',
+    'shift': 'G#4',
+    'space': 'A4',
+    'backspace': 'A#4',
+    'enter': 'B4',
+    'a': 'C5',
+    'b': 'C#5',
+    'c': 'D5',
+    'd': 'D#5',
+    'e': 'E5',
+    'f': 'F5',
+    'g': 'F#5',
+    'h': 'G5',
+    'i': 'G#5',
+    'j': 'A5',
+    'k': 'A#5',
+    'l': 'B5',
+    'm': 'C6',
+    'n': 'C#6',
+    'o': 'D6',
+    'p': 'D#6',
+    'q': 'E6',
+    'r': 'F6',
+    's': 'F#6',
+    't': 'G6',
+    'u': 'G#6',
+    'v': 'A6',
+    'w': 'A#6',
+    'x': 'B6',
+    'y': 'C7',
+    'z': 'C#7',
+    '0': 'D7',
+    '1': 'D#7',
+    '2': 'E7',
+    '3': 'F7',
+    '4': 'F#7',
+    '5': 'G7',
+    '6': 'G#7',
+    '7': 'A7',
+    '8': 'A#7',
+    '9': 'B7',
+    '-': 'C8',
+    'f1': 'C#8',
+    'f2': 'D8',
+    'f3': 'D#8',
+    'f4': 'E8',
+    'f5': 'F8',
+    'f6': 'F#8',
+    'f7': 'G8',
+    'f8': 'G#8',
+    'f9': 'A8',
+    'delete': 'A#8',
+    'f11': 'B8',
+    'f12': 'C9'
+}
+
+reverse_key_settings = {j: i for i, j in key_settings.items()}
+
+midi_device_id = 2
+
+frequency = 44100
+size = -16
+channel = 1
+buffer = 1024
+maxinum_channels = 100
+
+global_volume = 0.6
+
+delay_time = 6
+
+sound_format = 'wav'
+
+sound_path = 'sounds/'
